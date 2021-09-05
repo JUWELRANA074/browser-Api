e2a718773532bfcb52c2a293c65ac5a3b9091573
unknown <JUWELRANA074>
2021-09-05 07:46:22 +0600

    alert confirm prompt added

--- alert.js
+++ alert.js
@@ -1,1 +1,26 @@
-console.log('juwel');
+// alert 
+function maIsComming() {
+    alert('Taratari Porte bosss')
+}
+
+// confirm 
+const isGoing = () => {
+    const response = confirm('Are you ready for going to picnic')
+    if (response === true) {
+        alert('tk joma de akhoni ')
+    }
+    else {
+        console.log('DGM ! doew giya mor');
+    }
+}
+
+// prompt 
+const takeName = () => {
+    const name = prompt('What is Your Name')
+    if (name) {
+        alert('Thank you')
+    }
+    else {
+        alert('First tell me your Name')
+    }
+}
